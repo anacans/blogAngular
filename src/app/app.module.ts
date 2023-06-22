c21c6fbf4080666c458854e00fa48441a5720a4b
ANA CACERES <acaceresrueda@gmail.com>
2023-06-22 07:19:33 +0200

    ahora si creados los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaPostsComponent
+    ListaPostsComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
