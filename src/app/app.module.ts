99b7149c8eded5ac1a7bc29b8251e2da03b8d9a0
ANA CACERES <acaceresrueda@gmail.com>
2023-06-22 07:17:13 +0200

    instalado bootstrap y creados los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPostsComponent
   ],
   imports: [
     BrowserModule,
